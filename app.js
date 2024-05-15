aa421032022150ba98d90a33145cc0fd16bc3276
unknown <bibhishandhagate@gmail.com>
2024-05-15 11:11:30 +0530

    To add background color to webpage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<div style="background-color: #f0f0f0; padding: 20px;">Hello Bibhishan Dhagate, How are you?, YOu are great</div>'));
+app.get('/', (req, res) => res.send('<div style="background-color: #f0f0f0; padding: 20px;">Hello Bibhishan Dhagate, How are you?, You are awsome</div>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
