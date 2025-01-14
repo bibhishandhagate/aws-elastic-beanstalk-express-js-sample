0b028d58fbb596cd807e75c92501a5987161b6ba
bibhishandhagate <bibhishandhagate@gmail.com>
2025-01-14 07:32:48 +0530

    changed app.js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<div style="background-color: #0000FF; padding: 20px;">Hello Bibhishan good afternoon! </div>'));
+app.get('/', (req, res) => res.send('<div style="background-color: #0000FF; padding: 20px;">Hello Bibhishan good morning! </div>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
